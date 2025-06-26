3c56a1f1cd8f63e6d6410233777f3008615496a8
仲優樹 <yu-ki216m@outlook.jp>
2025-06-26 13:13:38 +0900

    Initial Commit

---
+++ AutoSuggestCombine/AutoSuggestCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSuggestCombineApp.swift
+//  AutoSuggestCombine
+//
+//  Created by 仲優樹 on 2025/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AutoSuggestCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
